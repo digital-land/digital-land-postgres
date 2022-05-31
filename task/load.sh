953a8018540bafec88e359f942dff8740bbedfba
Adam Shimali <adam.shimali@gmail.com>
2022-05-31 22:31:58 +0100

    revert load script

--- task/load.sh
+++ task/load.sh
@@ -26,31 +26,29 @@ DATABASE_NAME=${DATABASE%.*}
 
 URL="https://$S3_BUCKET.s3.eu-west-2.amazonaws.com/$S3_KEY"
 
-#if ! [ -f $DATABASE_NAME.sqlite3 ]; then
-#  echo "downloading from $URL"
-#  curl -O $URL
-#fi
-#
-#echo "exporting $DATABASE"
-#sqlite3 $DATABASE ".read sql/export_$DATABASE_NAME.sql"
-#
-#if [ -n "$CF_DATABASE_SERVICE_NAME" ]; then
-#  echo "load data into gov.uk PaaS postgres"
-#  # shellcheck disable=SC2016
-#  cf login -a api.london.cloud.service.gov.uk -u $CF_USER -p "$CF_PASSWORD" -s "$ENVIRONMENT"
-#  # Cloudfoundry conduit addon does some magic where it executes a process on your host machine that has access a particular cloudfoundry hosted service
-#  cf conduit "$CF_DATABASE_SERVICE_NAME" -- bash -c "python3 -m pgload.load_with_vcap_credentials --source=$DATABASE_NAME"
-#else
-#  echo "load data into RDS postgres"
-#  python3 -m pgload.load --source=$DATABASE_NAME
-#
-#  if [ $DATABASE_NAME == "entity" ]; then
-#    echo "loading facts"
-#    python3 -m pgload.load_facts --bucket=$S3_BUCKET
-#  fi
-#
-#fi
+if ! [ -f $DATABASE_NAME.sqlite3 ]; then
+  echo "downloading from $URL"
+  curl -O $URL
+fi
 
-python3 -m pgload.load_facts
+echo "exporting $DATABASE"
+sqlite3 $DATABASE ".read sql/export_$DATABASE_NAME.sql"
+
+if [ -n "$CF_DATABASE_SERVICE_NAME" ]; then
+  echo "load data into gov.uk PaaS postgres"
+  # shellcheck disable=SC2016
+  cf login -a api.london.cloud.service.gov.uk -u $CF_USER -p "$CF_PASSWORD" -s "$ENVIRONMENT"
+  # Cloudfoundry conduit addon does some magic where it executes a process on your host machine that has access a particular cloudfoundry hosted service
+  cf conduit "$CF_DATABASE_SERVICE_NAME" -- bash -c "python3 -m pgload.load_with_vcap_credentials --source=$DATABASE_NAME"
+else
+  echo "load data into RDS postgres"
+  python3 -m pgload.load --source=$DATABASE_NAME
+
+  if [ $DATABASE_NAME == "entity" ]; then
+    echo "loading facts"
+    python3 -m pgload.load_facts
+  fi
+
+fi
 
 echo "load done"
