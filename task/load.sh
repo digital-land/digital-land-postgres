3eb219f6149a2b8ab2455037b949e81ed3cedaab
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-04 12:58:52 +0000

    Initial preparation for new infra

--- task/load.sh
+++ task/load.sh
@@ -1,33 +1,23 @@
-#! /usr/bin/env sh
-# Environment Variables:
-# * S3_KEY
-# * S3_BUCKET
-# * DB_NAME
-# * DB_USER_NAME
-# * DB_PASSWORD
-# * DB_WRITE_ENDPOINT
-set -e
-
-if [ -f ./.env ]; then
-  export $(cat .env | xargs)
-fi
+#! /usr/bin/env bash
 
 echo "task running with env vars S3_BUCKET = $S3_BUCKET and S3_KEY = $S3_KEY"
 
 DATABASE=${S3_KEY##*/}
 DATABASE_NAME=${DATABASE%.*}
 
-URL="https://$S3_BUCKET.s3.eu-west-2.amazonaws.com/$S3_KEY"
-
 if ! [ -f $DATABASE_NAME.sqlite3 ]; then
-  echo "downloading from $URL"
-  curl -O $URL
+  echo "$EVENT_ID: attempting download from $COLLECTION_DATA_URL/$S3_KEY"
+  if [[ $(curl -sI $COLLECTION_DATA_URL/$S3_KEY | grep "200 OK") == *200* ]]; then
+    curl -sO $COLLECTION_DATA_URL/$S3_KEY || echo "$EVENT_ID: failed to download from $COLLECTION_DATA_URL/$S3_KEY"
+  else
+    echo "$EVENT_ID: failed to download from $COLLECTION_DATA_URL/$S3_KEY"
+  fi
 fi
 
-echo "exporting $DATABASE"
-sqlite3 $DATABASE ".read sql/export_$DATABASE_NAME.sql"
+echo "$EVENT_ID: exporting $DATABASE"
+sqlite3 $DATABASE ".read sql/export_$DATABASE_NAME.sql" || echo "$EVENT_ID: failed to export $DATABASE"
 
-echo "load data into RDS postgres"
-python3 -m pgload.load --source=$DATABASE_NAME
+echo "$EVENT_ID: load data into RDS postgres"
+python3 -m pgload.load --source=$DATABASE_NAME || echo "$EVENT_ID: failed to load $DATABASE"
 
-echo "load done"
+echo "$EVENT_ID: loading of database $DATABASE_NAME completed successfully"
