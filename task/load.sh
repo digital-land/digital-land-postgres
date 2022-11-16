3f476f60203c6c059a5129d77777a5c6d76dcfe2
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-16 13:30:03 +0000

    Fix: exit or does not work with aws cli

--- task/load.sh
+++ task/load.sh
@@ -7,10 +7,14 @@ echo "$EVENT_ID: running with settings: S3_BUCKET=$S3_BUCKET, S3_KEY=$S3_KEY, DA
 
 if ! [ -f "$DATABASE_NAME.sqlite3" ]; then
   echo "$EVENT_ID: attempting download from s3://$S3_BUCKET/$S3_KEY"
-  aws s3api get-object --bucket "$S3_BUCKET" --key "$S3_KEY" "$DATABASE_NAME.sqlite3" > /dev/null || \
-    echo "$EVENT_ID: failed to download from s3://$S3_BUCKET/$S3_KEY" && exit 1
+  aws s3api get-object --bucket "$S3_BUCKET" --key "$S3_KEY" "$DATABASE_NAME.sqlite3" > /dev/null
 
-  echo "$EVENT_ID: finished downloading from s3://$S3_BUCKET/$S3_KEY"
+  if ! [ -f "$DATABASE_NAME.sqlite3" ]; then
+    echo "$EVENT_ID: failed to download from s3://$S3_BUCKET/$S3_KEY"
+    exit 1
+  else
+    echo "$EVENT_ID: finished downloading from s3://$S3_BUCKET/$S3_KEY"
+  fi
 fi
 
 echo "$EVENT_ID: extracting data from $DATABASE"
