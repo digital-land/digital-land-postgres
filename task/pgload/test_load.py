c66e399bfc79ebdb142ea80042ba9c83e4fadf4b
Kena Vyas <kvyas@scottlogic.com>
2023-03-31 15:09:05 +0100

    db mock

--- task/pgload/test_load.py
+++ task/pgload/test_load.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import pytest
 import psycopg2
@@ -14,6 +15,8 @@ conn = psycopg2.connect(host=host, database=database, user=user, password=passwo
 
 def test_connect():
     assert conn.status == psycopg2.extensions.STATUS_READY
+
+
 
 @pytest.fixture(scope="module")
 def source():
@@ -69,7 +72,44 @@ def test_do_replace(source):
             rowcount = cursor.fetchone()[0]
             assert rowcount == 0
 
+def test_postgres(postgresql):
 
+    cur = postgresql.cursor()
+    cur.execute("CREATE TABLE entity (entity bigint,name varchar,entry_date varchar,start_date varchar,end_date varchar,dataset varchar,json varchar,organisation_entity varchar,prefix varchar,reference varchar,typology varchar,geometry varchar,point varchar)")
+    
+    with open('entities.csv', 'r') as f:
+      
+        reader = csv.reader(f,delimiter='|')
+        next(reader)  
+        for row in reader:
+            cur.execute("INSERT INTO entity (entity, name, entry_date, start_date, end_date, dataset, json, organisation_entity, prefix, reference, typology, geometry, point) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", row)
 
+    postgresql.commit()
+    cur.execute("SELECT * FROM entity limit 11")
+    result = cur.fetchone()
+    print(result)  
+    assert len(result)<0
+    cur.close()
 
+    #cur.execute("INSERT INTO entity (id, name) VALUES (1, 'John')")
+    # assert result == (1, 'John')
+    # postgresql.commit()
+    
+
+# postgresql = factories.postgresql()
+
+# # @pytest.fixture(scope='session')
+# # def postgresql_database(postgresql):
+# #     return postgresql
+
+# def test_insert_data(postgresql):
+#     conn = psycopg2.connect(**postgresql_database.dsn())
+#     cur = conn.cursor()
+#     cur.execute("INSERT INTO entity (id, name) VALUES (1, 'John')")
+#     conn.commit()
+#     cur.execute("SELECT * FROM entity")
+#     result = cur.fetchone()
+#     assert result == (1, 'John')
+#     cur.close()
+#     conn.close()
 
