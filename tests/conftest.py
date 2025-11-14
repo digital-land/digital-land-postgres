39eb9082079d9a05ab16b1687e6f9559ed0f2e86
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2025-11-14 22:11:03 +0000

    add quallity

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,7 +41,8 @@ def create_db(postgresql_conn):
             typology varchar,
             geojson varchar,
             geometry geometry null,
-            point varchar
+            point varchar,
+            quality varchar null
         )"""
     )
     cursor.execute(
